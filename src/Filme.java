76704d845bed7090bb1118e310be0b0bdf2d24f0
aquinolu25 <quinnpw@live.com>
2026-01-08 20:44:00 -0300

    Feito alguns testes com modificadores de acesso, e aprendendo a utilizar getters e setters

--- src/Filme.java
+++ src/Filme.java
@@ -3,9 +3,13 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
 
     void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome);
