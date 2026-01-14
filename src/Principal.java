591954a43221a358e17c953d3ad03b4c60820313
aquinolu25 <quinnpw@live.com>
2026-01-14 20:31:35 -0300

    Feito alguns testes com sobrescrita de metodo

--- src/Principal.java
+++ src/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
         meuFilme.setNome("Sinners");
         meuFilme.setAnoDeLancamento(2025);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -22,5 +23,7 @@ public class Principal {
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
     }
 }
