6539502efae88bf8a19158aefb8fd862559059fe
aquinolu25 <quinnpw@live.com>
2026-01-17 14:38:10 -0300

    Criado classe FiltroRecomendacao e adicionado novas funcionalidades ao projeto.

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.aquino.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.aquino.screenmatch.calculos.FiltroRecomendacao;
+import br.com.aquino.screenmatch.modelos.Episodio;
 import br.com.aquino.screenmatch.modelos.Filme;
 import br.com.aquino.screenmatch.modelos.Serie;
 
@@ -37,5 +39,14 @@ public class Principal {
         calculadora.inclui(outroFilme);
         calculadora.inclui(lost);
         System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 }
