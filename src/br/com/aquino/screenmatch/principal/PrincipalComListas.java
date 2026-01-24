8c8ab41aef1434db74fb6e2a5ecb0513352a8645
aquinolu25 <quinnpw@live.com>
2026-01-24 11:36:13 -0300

    Feito testes com o metodo compareTo na classe PrincipalComListas

--- src/br/com/aquino/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/aquino/screenmatch/principal/PrincipalComListas.java
@@ -6,6 +6,7 @@ import br.com.aquino.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
 
@@ -43,6 +44,10 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtista);
         System.out.println("Lista de titulos ordenados: ");
         Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano: ");
         System.out.println(lista);
 
     }
